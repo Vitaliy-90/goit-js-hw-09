f0fae83bbfaa25c8edc12e7ba168922d6a0d4071
Vitaliy-90 <124288490+Vitaliy-90@users.noreply.github.com>
2023-06-23 15:08:17 +0000

    Deploying to gh-pages from @ Vitaliy-90/goit-js-hw-09@5143a744a8a0a26f57874a5a9e190b1b9adde0b0 🚀

---
+++ 01-color-switcher.4a4835b1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),a=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.4a4835b1.js.map
